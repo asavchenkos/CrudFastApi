0b3bc7f1ebc29820e0f245a21459f2f803112a09
potihoygrusti <andriisavchenko6@gmail.com>
2024-02-14 01:47:56 -0500

    Added Script for verification

---
+++ auth-script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Docker registry credentials
+DOCKER_USERNAME="rahowa"
+DOCKER_PASSWORD="Logitechc4"
+DOCKER_REGISTRY="docker.io"
+
+# Docker image details
+IMAGE_NAME="rahowa/fast_api_nginx"
+IMAGE_TAG="stable"
+
+# Authenticate with Docker
+echo "$DOCKER_PASSWORD" | docker login --username $DOCKER_USERNAME --password-stdin $DOCKER_REGISTRY
+
+# Build the Docker image
+docker build -t $DOCKER_REGISTRY/$IMAGE_NAME:$IMAGE_TAG .
+
+# Push the Docker image to the registry
+docker push $DOCKER_REGISTRY/$IMAGE_NAME:$IMAGE_TAG
