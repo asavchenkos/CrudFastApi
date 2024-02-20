d22198c72d8f1eb9b4cb4711f06c2f4078d4911e
potihoygrusti <andriisavchenko6@gmail.com>
2024-02-19 20:55:07 -0500

    test

--- auth-script.sh
+++ auth-script.sh
@@ -7,7 +7,7 @@ DOCKER_REGISTRY="docker.io"
 
 # Docker image details
 IMAGE_NAME="rahowa/fast_api_nginx"
-IMAGE_TAG="stable"
+IMAGE_TAG=$1
 
 # Authenticate with Docker
 echo "$DOCKER_PASSWORD" | docker login --username $DOCKER_USERNAME --password-stdin $DOCKER_REGISTRY
@@ -17,3 +17,5 @@ docker build -t $DOCKER_REGISTRY/$IMAGE_NAME:$IMAGE_TAG .
 
 # Push the Docker image to the registry
 docker push $DOCKER_REGISTRY/$IMAGE_NAME:$IMAGE_TAG
+
+echo "Docker image pushed to $DOCKER_REGISTRY/$IMAGE_NAME:$IMAGE_TAG"
